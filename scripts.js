cda6daf1ee11c72f06e19ad5731e570f42c046bc
Alexandra Nelson <alexandra.v.nelson@gmail.com>
2023-10-30 16:35:14 -0700

    Updated layout

--- scripts.js
+++ scripts.js
@@ -1,13 +1,2 @@
-document.getElementById('profilePicture').addEventListener('change', function(event) {
-    const preview = document.getElementById('profilePicturePreview');
-    const file = event.target.files[0];
-    if (file) {
-        const reader = new FileReader();
-        reader.onload = function(e) {
-            preview.src = e.target.result;
-        };
-        reader.readAsDataURL(file);
-    }
- });
- 
+
  
